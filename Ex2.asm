272b8556c94a44b8786755b55c218960c43c69e8
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-05-09 20:31:55 +0100

    the first solution

--- Ex2.asm
+++ Ex2.asm
@@ -1,1 +1,10 @@
 ;Ecrire un programme qui fait la somme des entiers de 1 à 16 et stocker dans la case mémoire 1100
+
+MOV AX , 0
+MOV CX , 16
+somme : ADD AX , CX
+DEC CX
+CMP CX , 0
+JNZ somme
+MOV [1100] , AX
+int 20
