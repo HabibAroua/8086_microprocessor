7d66c75cd520f5a82333d0b398e02ba22372d7aa
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-05-14 22:55:21 +0100

    edit code

--- arith.asm
+++ arith.asm
@@ -2,28 +2,28 @@
 ;L'addition et soustraction ont le même principe
 ;Multiplication
 
-mov al,51
-mov bl,32 ; ➔AX = 51* 32
-mul bl
+MOV al,51
+MOV bl,32 ; ➔AX = 51* 32
+MUL bl
 
-mov ax,4253
-mov bx,1689 ;➔ (DX,AX)=4253*1689
-mul bx
+MOV ax,4253
+MOV bx,1689 ;➔ (DX,AX)=4253*1689
+MUL bx
 
-mov al,43
-mov byte [1200H],28 ;➔ AX=43*28
-mul byte [1200H]
+MOV al,43
+MOV byte [1200H],28 ;➔ AX=43*28
+MUL byte [1200H]
 
-mov ax,1234
-mov word [1200H],5678 ;➔ (DX,AX)=1234*5678
-mul word [1200H]
+MOV ax,1234
+MOV word [1200H],5678 ;➔ (DX,AX)=1234*5678
+MUL word [1200H]
 
 ;Division
-mov ax,35
-mov bl,10 ;➔ AL=3 (quotient) et AH=5 (reste)
-div bl
+MOV ax,35
+MOV bl,10 ;➔ AL=3 (quotient) et AH=5 (reste)
+DIV bl
 
-mov dx,0
-mov ax,1234 ;➔ AX=123 (quotient) et DX=4 (reste)
-mov bx,10
-div bx
+MOV dx,0
+MOV ax,1234 ;➔ AX=123 (quotient) et DX=4 (reste)
+MOV bx,10
+DIV bx
