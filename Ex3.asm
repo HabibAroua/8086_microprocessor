5169adba64bb66717dcfe7d5a0f7ea281083520c
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-05-14 18:52:46 +0100

    edit content

--- Ex3.asm
+++ Ex3.asm
@@ -1,5 +1,5 @@
-;Excrire un programme qui teste ke bit 7 de AL 
-;1) Si le bit 7 de AL=0 , faire un seuk décalage à droite de BX de 15 position
+;Excrire un programme qui teste que bit 7 de AL 
+;1) Si le bit 7 de AL=0 , faire un seut décalage à droite de BX de 15 position
 ;2) Si le bit 7 de AL=1 , faire une série de 15 décalage à gauche de 1 bit chacun
 
 AND AL , 80
